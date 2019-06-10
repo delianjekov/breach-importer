c709bf51adfa7d52435587432dd0eb72679223cd
Delian Jekov <delian_jekov@mail.bg>
2019-06-10 17:29:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -36,33 +36,43 @@ namespace BreachImporter
             var mysqlPassword = app.Option("-p|--password",
                 "The MySql password to use for the import process", CommandOptionType.SingleValue);
 
-            app.OnExecute(() => { 
-                var directory = new DirectoryInfo(breachCompilationDataPath.Value());
-                foreach (var fileInfo in directory.GetFiles("*"))
-                {
-                    var records = new List<KeyValuePair<string, string>>();
-                    using (var file = new StreamReader(fileInfo.FullName))
-                    {
-                        string line;
-                        while ((line = file.ReadLine()) != null)
-                        {
-                            var chunks = line.Split(new[] {":", ";"}, 2, StringSplitOptions.RemoveEmptyEntries);
-                            if (chunks.Length == 2)
-                            {
-                                var user = Escape(chunks[0]);
-                                var pass = Escape(chunks[1]);
+            app.OnExecute(() => {
 
-                                records.Add(new KeyValuePair<string, string>(user, pass));
+                
+                if (app.Options.Any(o => !o.HasValue()))
+                {
+                    app.ShowHint();
+                }
+                else
+                {
+                    var directory = new DirectoryInfo(breachCompilationDataPath.Value());
+                    foreach (var fileInfo in directory.GetFiles("*"))
+                    {
+                        var records = new List<KeyValuePair<string, string>>();
+                        using (var file = new StreamReader(fileInfo.FullName))
+                        {
+                            string line;
+                            while ((line = file.ReadLine()) != null)
+                            {
+                                var chunks = line.Split(new[] {":", ";"}, 2, StringSplitOptions.RemoveEmptyEntries);
+                                if (chunks.Length == 2)
+                                {
+                                    var user = Escape(chunks[0]);
+                                    var pass = Escape(chunks[1]);
+
+                                    records.Add(new KeyValuePair<string, string>(user, pass));
+                                }
                             }
                         }
+
+                        var query = $"INSERT INTO {mysqlTable.Value()}(user, pass) VALUES ";
+                        query += string.Join(",", records.Select(r => $"('{r.Key}','{r.Value}')"));
+
+                        var passwordString = mysqlPassword.HasValue() ? $"-p {mysqlPassword.Value()}" : string.Empty;
+                        var command =
+                            $"{query} | mysql -u {mysqlUsername.Value()} {passwordString} {mysqlDatabase.Value()}";
+                        ExecuteBashCommand(command);
                     }
-
-                    var query = $"INSERT INTO {mysqlTable.Value()}(user, pass) VALUES ";
-                    query += string.Join(",", records.Select(r => $"('{r.Key}','{r.Value}')"));
-
-                    var passwordString = mysqlPassword.HasValue() ?  $"-p {mysqlPassword.Value()}" : string.Empty;
-                    var command = $"{query} | mysql -u {mysqlUsername.Value()} {passwordString} {mysqlDatabase.Value()}";
-                    ExecuteBashCommand(command);
                 }
 
                 return 0;
